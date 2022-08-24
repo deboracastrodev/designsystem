a25648ca2fbbd9a82dabe373c482937931f4ba25
Debora Castro <debora@vert-capital.com>
2022-08-24 19:59:41 -0300

    import components from legacy repository

---
+++ src/components/button/VButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button :class="[setSizeClass, setStatus]" :disabled="disabled">
+    <div v-if="icon" class="v-btn--img">
+      <img :src="setIcon" />
+    </div>
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VButton",
+  props: {
+    size: {
+      type: String,
+      default: "md",
+    },
+    outline: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+      default: "primary",
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    setSizeClass(): string {
+      return `v-btn--${this.size}`;
+    },
+    setStatus(): string {
+      const status = this.outline
+        ? `v-btn__outline--${this.status}`
+        : `v-btn--${this.status}`;
+      return status;
+    },
+    setIcon(): string {
+      return `/static/icons/${this.icon}.svg`;
+    },
+  },
+});
+</script>
+
+<style src="./VButton.scss" lang="scss" />
